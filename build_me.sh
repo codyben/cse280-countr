78fd173b6d738ac497b13c76f79221decc208d98
Cody Benkoski <cob322@lehigh.edu>
2020-02-14 11:19:33 -0500

    Make sure conan gets deps

--- build_me.sh
+++ build_me.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+conan remote add stiffstream https://api.bintray.com/conan/stiffstream/public
+# Add remote for conan to find RESTinio's dependencies.
+conan remote add public-conan https://api.bintray.com/conan/bincrafters/public-conan  
 mkdir -p build/ && cd build && conan install .. --build=missing && cmake .. && cmake --build . --config Release && ./bin/hello_world_minimal
